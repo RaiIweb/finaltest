55d0896c24e20a3b91f4d4ddeaeeb99e795ddb90
RaiIweb <rai.iwebcode@gmail.com>
2021-11-13 21:29:06 +0530

    success

--- src/utils/upDateScore.js
+++ src/utils/upDateScore.js
@@ -1,12 +1,14 @@
 
 export default async function updateScore(status) {
 
-    await fetch("https://gameback-end.herokuapp.com/update", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-          },
-        body: JSON.stringify({status})
-      })
+    let result  = await fetch("https://gameback-end.herokuapp.com/update", {
+                        method: "POST",
+                        credentials: "include",
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8'
+                        },
+                        body: JSON.stringify({status})
+                    })
+    let response = result.json()
+    console.log(response);
 }
