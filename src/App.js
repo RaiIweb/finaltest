b83b7a65700021bf9780b2375d69a4fd205b5628
RaiIweb <rai.iwebcode@gmail.com>
2021-11-15 11:48:09 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Game from "./Components/Game";
-import Login from "./Components/Login";
-import Signup from "./Components/Signup";
+import Game from "./components/Game";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
