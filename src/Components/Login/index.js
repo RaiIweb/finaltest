aa5e58f86d60ee0cdcc78632c82f501be01afb1b
RaiIweb <rai.iwebcode@gmail.com>
2021-11-15 11:28:47 +0530

    success

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -55,7 +55,7 @@ function Login() {
         </div>
 
         <div className="container" style={{ backgroundColor: "#f1f1f1" }}>
-          <span className="psw">
+          <span className="">
             <Link to="/signup">Sign up</Link>
           </span>
         </div>
